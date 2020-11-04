64098670c88561a2004ca8d33845488ccdf429d5
thomas-oss <thomas.risi@cpe.fr>
2020-11-04 11:18:04 +0100

    Initialisation

--- Serveur/main.c
+++ Serveur/main.c
@@ -14,43 +14,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdio.h>
 #include <pthread.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
 #include <stdbool.h>
 
-#include "clientcxnmanager.h"
-
+#include "srvcxnmanager.h"
 
 /*
- * 
+ *
  */
+
 int main(int argc, char** argv) {
-
-
-    int sockfd;
-    int status = 0;
-    char msg[100];
+    int sockfd = -1;
+    int index = 1;
+    connection_t *connection;
     pthread_t thread;
 
-    sockfd = open_connection();
 
-    strcpy(msg, "Hello from Xeon"); //Xeon is the name of the this client
-    printf("sending : %s\n", msg);
-    write(sockfd, msg, strlen(msg));
+    /* init array*/
+    init_sockets_array();
+    /* create socket */
+    sockfd = create_server_socket();
 
-    //Creation d'un pthread de lecture
-    pthread_create(&thread, 0, threadProcess, &sockfd);
-    //write(connection->sock,"Main APP Still running",15);
-    pthread_detach(thread);
-    do {
-        fgets(msg, 100, stdin);
-        //printf("sending : %s\n", msg);
-        status = write(sockfd, msg, strlen(msg));
-        //memset(msg,'\0',100);
-    } while (status != -1);
+    /* listen on port , stack size 50 for incoming connections*/
+    if (listen(sockfd, 50) < 0) {
+        fprintf(stderr, "%s: error: cannot listen on port\n", argv[0]);
+        return -5;
+    }
 
+    printf("ready and listening\n");
+
+    //Wait for connection
+    while (true) {
+        /* accept incoming connections */
+        connection = (connection_t *) malloc(sizeof (connection_t));
+        connection->sockfd = accept(sockfd, &connection->address, &connection->addr_len);
+        connection->index = index++;
+        if (connection->sockfd <= 0) {
+            free(connection);
+        } else {
+            /* start a new thread but do not wait for it */
+            pthread_create(&thread, 0, threadProcess, (void *) connection);
+            pthread_detach(thread);
+        }
+    }
     return (EXIT_SUCCESS);
 }
 
